c17159c64433409cda18f63971d9b2915ae75198
nicholasss <nicholaszingleman@gmail.com>
2024-12-27 12:25:31 -0600

    added uniqueLetters func to check if there is a repeated letter

--- words.c
+++ words.c
@@ -3,12 +3,37 @@
 #include <string.h>
 
 void printHelp();
+int uniqueLetters(int strlen, char *letters);
 
 void printHelp() {
 	printf(" This program solves the spelling bee.\n");
 	printf(" Please provide the letters for searching.\n");
 	printf(" i.e. 'abcdef'\n");
 	printf(" Written by Nicholas Zingleman\n");
+}
+
+int uniqueLetters(int len, char *letters) {
+	if (len < 1 || len > 7) {
+		printf("Please check the number of letters once more.\n");
+		return 0; // false
+	} 
+	if (letters == NULL) {
+		// not sure letters will ever be null, but doesnt hurt to check.
+		printf("Unable to check letters, null pointer.\n");
+		return 0; // false
+	}
+
+	// compare each and every letter
+	int unique = 1; // remains true unless same letter found
+	for (int i = 0; i < len; i++) {
+		for (int j = 0; j < len; j++) {
+			if (letters[i] == letters[j]) {
+				unique = 0; // false
+			}
+		}
+	}
+	
+	return unique;
 }
 
 int main(int argc, char **argv) {
